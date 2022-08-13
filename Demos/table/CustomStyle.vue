861d98d941f298cdeee486bfd7331c0a7251fb13
xuzhicheng <xuzhicheng@xuzhichengdeMacBook-Pro.local>
2022-08-13 19:26:40 +0800

    fix: rewrite  table docs

--- Demos/table/CustomStyle.vue
+++ Demos/table/CustomStyle.vue
@@ -103,24 +103,33 @@ export default defineComponent({
       {
         title: 'Address',
         field: 'address',
+        width: '300px'
       },
     ]
 
     // 自定义表格的样式
-    const headerRowClass = () => 'outline-black'
-    const headerRowStyle = () => ({ fontSize: '22px' })
+    // 自定义表头 class
+    const headerRowClass = () => 'table-header'
+    // 自定义表头样式
+    const headerRowStyle = () => ({ height: '80px' })
+    // 自定义表头单元格 class
     const headerCellClassName = ({ colIndex }) =>
-      colIndex === 1 ? 'class-green' : 'class-green'
+      colIndex === 1 ? 'cell-sky' : 'cell-green'
+    // 自定义表头单元格样式
     const headerCellStyle = ({ colIndex }) =>
-      colIndex === 1 ? { fontSize: '22px' } : { color: 'blue' }
+      colIndex === 1 ? { color: 'rgba(5, 150, 105, 1)' } : { color: 'rgba(147, 197, 253, 1)' }
+    // 自定义行 class
     const rowClassName = ({ rowIndex }) =>
-      rowIndex === 1 ? 'row-min' : 'row-large'
+      rowIndex % 2 === 0 ? 'row-high' : 'row-low'
+    // 自定义行 样式
     const rowStyle = ({ rowIndex }) =>
-      rowIndex === 2 ? { fontSize: '16px' } : { fontSize: '14px' }
+      rowIndex % 2 === 1 ? { fontWeight: 700 } : { fontWeight: 100 }
+    // 自定义单元格 class
     const cellClassName = ({ rowIndex, colIndex }) =>
-      rowIndex === 1 && colIndex === 1 ? 'oneone' : 'others'
+      rowIndex === 1 && colIndex === 1 ? 'cell-pink' : ''
+    // 自定义单元格样式
     const cellStyle = ({ row, colIndex }) =>
-      colIndex === 1 && row.age > 20 ? { color: 'red' } : null
+      colIndex === 1 && row.age > 20 ? {backgroundColor: 'rgba(251, 191, 36, 1)'} : null
 
     return {
       data,
@@ -138,3 +147,24 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+.table-header{
+  height: 100px;
+}
+.cell-sky {
+  background-color: rgba(147, 197, 253, 1);
+}
+.cell-green {
+  background-color: rgba(5, 150, 105, 1)
+}
+.row-high {
+  height: 120px;
+}
+.row-low {
+  height: 70px;
+}
+.cell-pink {
+  color: rgba(244, 114, 182, 1)
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,7 @@ import './resetcss/normalize.css'
 import Theme from 'vitepress/dist/client/theme-default'
 import { registerComponents } from './register-components'
 import { setupApathiaTwindTheme } from '@apathia/apathia'
-import { toastInstall, modalInstall } from '@apathia/apathia'
+import { toastInstall, modalInstall, } from '@apathia/apathia'
 import './resetcss/color.css'
 
 export default {
@@ -13,6 +13,72 @@ export default {
     setupApathiaTwindTheme(() => ({
       preflight: false,
     }))
+    // setupApathiaTwindTheme(({ colors, twind}) => {
+    //   const styleEl = document.createElement('style')
+    //   styleEl.id = '__mng__'
+    //   document.head.insertBefore(styleEl, document.head.firstChild)
+    //   if (!styleEl.sheet) {
+    //     throw new Error('无法获取样式')
+    //   }
+    //   const sheet = twind.cssomSheet({ target: styleEl.sheet })
+    //   return {
+    //     sheet,
+    //     preflight: false,
+    //     theme: {
+    //       colors: {
+    //         black: '#000',
+    //         white: '#fff',
+    //         transparent: 'transparent',
+    //         red: {
+    //           100: colors.red['100'],
+    //           300: colors.red['300'],
+    //           500: colors.red['500'],
+    //           700: colors.red['700'],
+    //           900: colors.red['900'],
+    //         },
+    //         yellow: {
+    //           100: colors.yellow['100'],
+    //           300: colors.yellow['300'],
+    //           500: colors.yellow['500'],
+    //           700: colors.yellow['700'],
+    //           900: colors.yellow['900'],
+    //         },
+    //         green: {
+    //           100: colors.green['100'],
+    //           300: colors.green['300'],
+    //           500: colors.green['500'],
+    //           700: colors.green['700'],
+    //           900: colors.green['900'],
+    //         },
+    //         gray: {
+    //           100: colors.coolGray['100'],
+    //           300: colors.coolGray['300'],
+    //           500: colors.coolGray['500'],
+    //           700: colors.coolGray['700'],
+    //           900: colors.coolGray['900'],
+    //         },
+    //         brand: {
+    //           100: colors.blue['100'],
+    //           300: colors.blue['300'],
+    //           500: colors.blue['500'],
+    //           700: colors.blue['700'],
+    //           900: colors.blue['900'],
+    //         },
+    //       },
+    //       zIndex: {
+    //         0: '0',
+    //         10: '10',
+    //         20: '20',
+    //         30: '30',
+    //         40: '40',
+    //         50: '50',
+    //         modal: '900',
+    //         dropdown: '950',
+    //         alert: '999',
+    //       },
+    //     },
+    //   }
+    // })
 
     app.use(toastInstall)
     app.use(modalInstall)
